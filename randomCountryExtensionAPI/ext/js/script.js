3084a73108bf8a2dde808df6c249d3d356324438
Mads <mads.heilberg@gmail.com>
2017-11-07 23:22:04 +0100

    Add files via upload

---
+++ randomCountryExtensionAPI/ext/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function (){
+var countryURL = "http://localhost/REST/api.php/country/";
+var cityURL = "http://localhost/REST/api.php/city/";
+
+mainfunction();
+
+    function mainfunction(){
+    $.get(countryURL, function(data){
+        //parse JSON data
+        var countryData = JSON.parse(data);
+        //get maxIndex from data array
+        var maxIndex = countryData.length;
+        //generate random number between 1 and maxIndex
+        var ranNum = Math.floor((Math.random() * maxIndex) + 1);
+        //get info on random country
+        var country = countryData[ranNum].Name;
+        var continent = countryData[ranNum].Continent;
+        var pop = countryData[ranNum].Population;
+        var gnp = countryData[ranNum].GNP;
+
+            //get capital number and JSON data for that city
+            var capitalNum = countryData[ranNum].Capital;
+            $.get((cityURL + capitalNum), function(data2){
+                var capitalData = JSON.parse(data2);
+                var capital = capitalData.Name;
+
+                //get other cities and ignore capital
+                $.get(cityURL, function(data3){
+                    cityData = JSON.parse(data3);
+                    var citiesArray = [];
+                    for (var i = 0; i < cityData.length; i++) {
+                        if (cityData[i].CountryCode == countryData[ranNum].Code){
+                            if(cityData[i].ID != capitalNum){
+                                citiesArray.push(cityData[i].Name);
+                            }
+                        }
+                    }
+
+                    //setup infotext
+                    var infoText = "<h3>" + country + "</h3>";
+                    infoText += "<p>Continent: " + continent + "</p>";
+                    infoText += "<p>Population: " + pop + "</p>";
+                    infoText += "<p>GNP: " + gnp + "</p>";
+                    infoText += "<p>Capital: " + capital + "</p>";
+                    infoText += "<p>Other major cities:";
+
+                    citiesArray.forEach(function(element, index) {
+                        infoText += " " + element;
+                        if(citiesArray[index + 1]){
+                            infoText += ",";
+                        }
+                    }, this);
+
+                    infoText += "</p>";
+        
+                    //append to infoText div
+                    $("#infoText").empty();
+                    $("#infoText").append(infoText);
+                });
+            });
+        });
+    }
+
+    $("#newButton").click(function (){
+        mainfunction();
+    });
+});
---
+++ randomCountryExtensionAPI/readme.txt
@@ -0,0 +1,21 @@
+﻿BY: Martin og Mads
+
+REQUIREMENTS:
+MAMP stack or equivalent
+Google Chrome
+
+<!--Install instructions for Windows--!>
+1. Launch MAMP and run Apache + MySQL servers 
+2. Import world.sql into MySQL server through phpMyAdmin.
+3. Copy/move REST folder to "...\MAMP\htdocs\" folder (API is now accessed as "localhost/REST/api.php/.../").
+4. GOTO "chrome://extensions" in Chrome browser and load unpacked extension -> point to "...\randomCountryExtension\ext\" path.
+5. Open new tab in chrome and enjoy the boredom!
+
+<!--Install instructions for Mac--!>
+1. Really bro? ¯\_(ツ)_/¯
+2. Translate the install steps for Windows to your inferior OS.
+3. Enjoy!
+
+<!--Install instructions for Linux--!>
+1. You honestly don't need me to tell you what to do!
+2. Enjoy!
